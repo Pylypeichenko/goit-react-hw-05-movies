27488a5b7c0152131e05e294c8ff885c0444c4dc
Pylypeichenko <99480889+Pylypeichenko@users.noreply.github.com>
2022-11-13 20:58:54 +0000

    Deploying to gh-pages from @ Pylypeichenko/goit-react-hw-05-movies@2feb18ad433166db48902d0f99284ea68c3a3837 🚀

---
+++ static/js/877.8df4cc81.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[877],{877:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,a=t(731),s=t(168),u=t(444).ZP.p(r||(r=(0,s.Z)(["\n  display: flex;\n  justify-content: center;\n"]))),c=t(184),o=function(){return(0,c.jsxs)(u,{children:["This page was not found. Please return to",(0,c.jsx)(a.rU,{to:"/",children:"main page"})]})}}}]);
+//# sourceMappingURL=877.8df4cc81.chunk.js.map
